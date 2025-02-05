109ee50e8a22b2f8c31e625ed8c36159861f2927
Irina <constance100478@gmail.com>
2025-02-05 12:42:47 +0200

    add hw_1

---
+++ Homeworks/hw_1/main.js
@@ -0,0 +1,74 @@
+console.log('Task 1');
+// #dYQNrBV
+// Створити змінні.
+// Присвоїти кожному з них значення: 'hello','owu','com', 'ua',
+// 1, 10, -999, 123, 3.14, 2.7, 16,
+// true, false.
+// Вивести кожну змінну за допомогою: console.log
+
+let asd = 'hello';
+console.log(asd);
+let asd1 = 'owu';
+console.log(asd1);
+let asd2 = 'com';
+console.log(asd2);
+let asd3 = 'ua';
+console.log(asd3);
+
+let qwe1 = 1;
+console.log(qwe1);
+let qwe2 = 10;
+console.log(qwe2);
+let qwe3 = -999;
+console.log(qwe3);
+let qwe4 = 123;
+console.log(qwe4);
+const PI = 3.14;
+console.log(PI);
+let qwe5 = 2.7;
+console.log(qwe5);
+let qwe6 = 16;
+console.log(qwe6);
+
+let bool1 = 'true';
+console.log(bool1);
+let bool2 = 'false';
+console.log(bool2);
+
+
+
+console.log('Task 2');
+// #6Qb97gsv
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+// З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName= 'Ірина';
+let middleName = 'Костянтинівна';
+let lastName = 'Кульбака';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+let person2 =`${firstName} ${middleName} ${lastName}`;
+console.log(person);
+console.log(person2);
+
+console.log('Task 3');
+// #4N0y5tufA
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100; let b = '100'; let c = true;
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a, a);
+console.log(typeof b, b);
+console.log(typeof c, c);
+
+
+console.log('Task 4');
+// #ruUtWDUI
+// Додаткове для тих хто цікавився prompt`oм
+// За допомогою 3х різних prompt() отримати 3 слова,
+// які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+let person3 = prompt('Enter your name');
+let person4 = prompt('Enter your middleName');
+let person5 = prompt('Enter your age');
+console.log(person3);
+console.log(person4);
+console.log(person5);
