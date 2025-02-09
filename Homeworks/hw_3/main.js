109186ed7aef62b24729ceccc6b1927a6b24c588
Irina <constance100478@gmail.com>
2025-02-09 08:35:38 +0200

    add hw_3

--- Homeworks/hw_3/main.js
+++ Homeworks/hw_3/main.js
@@ -107,14 +107,14 @@ let products = [
         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
     },
 ];
-//
+
 // ШАБЛОН
 // <div class="product-card">
 //     <h3 class="product-title">TITLE. Price - PRICE</h3>
 // <img src="IMAGE" alt="" class="product-image">
 // </div>
 // Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
-
+//
 // --------------------
 
 for (const product of products) {
